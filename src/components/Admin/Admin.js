789a8cb9af209f54f2655ec333ba3d121887284c
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-11-02 12:27:00 -0600

    basic structure of admin.js outlined

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -6,12 +6,30 @@ import Select from '@material-ui/core/Select';
 
 class Admin extends Component {
     
+    state = {
+        // Here goes all of the reviews from the database
+    }
+
+    // Here goes the axios for GET, PUT, & DELETE
 
     render(){
         return(
             
                 <div>
-                    
+                    <table>
+                        <tr>
+                            <th>ID</th>
+                            <th>Feeling</th>
+                            <th>Understanding</th>
+                            <th>Support</th>
+                            <th>Comments</th>
+                            <th>Flagged</th> {/* Here goes a PUT Req */}
+                            <th>Date</th>
+                            <th>Delete</th> {/* Here goes a DELETE Req */}
+                            
+                        </tr>
+                        
+                    </table>
                 </div>
             
         )
