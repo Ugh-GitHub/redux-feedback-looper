088c31ab7e70744c9d05b872a867f719e3ed8ded
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-11-01 19:19:07 -0600

    initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Feeling from '../Feeling/Feeling';
+import Comments from '../Comments/Comments';
+import Support from '../Support/Support';
+import Understanding from '../Understanding/Understanding';
+import Review from '../Review/Review';
+import Submitted from '../Submitted/Submitted';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br/>
+          {/* Routes go here */}
+          {/* <Route exact path="/feeling" component={Feeling} /> */}
+        </div>
+      </Router>
     );
   }
 }
